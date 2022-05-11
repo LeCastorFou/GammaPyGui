b6d0cbfaa687a688963368ddeca8fd5b11451d20
frnbrun <frn.brun@gmail.com>
2022-05-11 16:06:06 +0200

    Text button modifs

--- Main/BackEnd/main/forms.py
+++ Main/BackEnd/main/forms.py
@@ -15,7 +15,7 @@ class StartAnalysis(FlaskForm):
     rmin = IntegerField('Run min  *', validators=[DataRequired()],default=1)
     rmax = IntegerField('Run Max *', validators=[],default=200000)
     obs = SelectField('Observatory : ', choices=[('H.E.S.S.','H.E.S.S.'),('CTA','CTA'),('FERMI','FERMI')] , validators=[DataRequired()])
-    submit = SubmitField('Launch analysis')
+    submit = SubmitField('Prepare data analysis')
 
 class StartHessAna(FlaskForm):
     source =  SelectField('Choose a source', choices=[('','')] , validators=[])
@@ -25,7 +25,7 @@ class StartHessAna(FlaskForm):
     distance = FloatField('Maximun distance of run pointing *', validators=[DataRequired()],default=2)
     rmin = IntegerField('Run min  *', validators=[DataRequired()],default=1)
     rmax = IntegerField('Run Max *', validators=[],default=200000)
-    submit = SubmitField('Launch analysis')
+    submit = SubmitField('Setup analysis')
 
 class StartHess2D(FlaskForm):
     analysis =  SelectField('Select an ongoing Analysis', choices=[('','')] , validators=[])
